86a93246780b25b326aaeae4e4151a74327eb0b8
Shrushti <shrushtishree04@gmail.com>
2025-07-25 06:03:52 +0000

    new file

---
+++ examples/Assignment.py
@@ -0,0 +1,15 @@
+# Assignment Operators
+
+x = 5   # Basic Assignment
+
+# Addition Assignment
+y = 10
+y += 3  # Equivalent to y = y + 3
+
+
+
+# Task 2: Assignment Operators
+'''Create a variable total and initialize it to 10.
+Use assignment operators (+=, -=, *=, /=) to update the value of total.
+Print the final value of total.'''
+total = 10
