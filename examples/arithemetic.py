0101a44914141544d7d7be3a9ab155fff6bdddad
Shrushti <shrushtishree04@gmail.com>
2025-07-25 05:31:48 +0000

    new file

---
+++ examples/arithemetic.py
@@ -0,0 +1,44 @@
+# ARITHEMETIC OPERATOR 
+a = 5
+b = 3
+result = a + b
+print('addition of', a, b, ':', result)  # Output: 8
+
+x = 10
+y = 7
+result = x - y
+print('subtraction of', x, y, ':',result)  # Output: 3
+
+
+# Story: You're calculating your online shopping bill — adding item prices, applying discounts, calculating tax.
+a = 10
+b = 3
+
+print(a + b)   # 13 (Addition)
+print(a - b)   # 7 (Subtraction)
+print(a * b)   # 30 (Multiplication)
+print(a / b)   # 3.333... (Division)
+print(a // b)  # 3 (Floor Division)
+print(a % b)   # 1 (Modulus - remainder)
+print(a ** b)  # 1000 (Exponentiation)
+
+
+                                            # Task 1: Arithmetic Operators
+'''Create two variables with numeric values.
+Calculate the sum, difference, product, and quotient of a and b.
+Print the results.'''
+
+Aryan = 45
+Meera = 30
+
+total = Aryan + Meera
+print('altgether they have ', total ,'cakes')
+
+diff = Aryan - Meera
+print('aryan has ', diff, 'more cakes than meera')
+
+product = Aryan * Meera
+print('if meera has to deliver 45 cakes to 30 person they need: ', product, 'cakes')
+
+quotent = Aryan/Meera
+print('Aryan has',quotent, 'times more cakes than Meera.')
