fba5f77c5dca07c54c721cbd136d9016f07fde83
Shrushti <shrushtishree04@gmail.com>
2025-07-27 04:19:09 +0000

    new changes

---
+++ examples/Bitwise.py
@@ -0,0 +1,21 @@
+# Bitwise Operators
+a = 5     # Binary: 0101
+b = 3     # Binary: 0011
+
+# Bitwise AND (&)
+print(a & b)  # 0101 & 0011 = 0001 → Output: 1
+
+# Bitwise OR (|)
+print(a | b)  # 0101 | 0011 = 0111 → Output: 7
+
+#  Bitwise XOR (^)
+print(a ^ b)  # 0101 ^ 0011 = 0110 → Output: 6
+
+# Bitwise NOT (~)
+print(~a)     # ~5 → -(5 + 1) = -6
+
+# Left Shift (<<)
+print(a << 1) # 0101 << 1 = 1010 → Output: 10
+
+# Right Shift (>>)
+print(a >> 1) # 0101 >> 1 = 0010 → Output: 2
