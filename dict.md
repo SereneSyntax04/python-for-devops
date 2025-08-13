e835992eef6151f925e6a9bcd4560153fe5de852
Shrushti <shrushtishree04@gmail.com>
2025-08-13 12:33:48 +0000

    new

---
+++ dict.md
@@ -0,0 +1,22 @@
+# Think of a dictionary like a real-life dictionary:
+- You look up a word (the key)
+- You get its meaning (the value)
+
+##  What is a Dictionary?
+- Key-value pairs: Each item has a key (unique) and a value.
+- Keys can be strings, numbers, or tuples (immutable types).
+- Values can be anything: numbers, strings, lists, even other dictionaries.
+- Created using {} or the dict() constructor.
+
+## Basic Syntax:
+```python
+my_dict = {
+    "name": "Shrushti",
+    "age": 21,
+    "skills": ["Python", "AWS", "DevOps"]
+}
+```
+Here:
+- "name", "age", "skills" → keys
+- "Shrushti", 21, ["Python", "AWS", "DevOps"] → values
+[Examples]()
---
+++ examples/dict.py
@@ -0,0 +1,52 @@
+
+'''
+Keys must be unique and immutable.
+Values can be any Python object.
+Access time is fast because dictionaries use hash tables.
+'''
+
+
+# Basic Syntax and operations
+
+my_dict = {
+    "name": "Shrushti",
+    "age": 21,
+    "skills": ["Python", "AWS", "DevOps"]
+}
+# Accessing Values
+print(my_dict)
+print(my_dict["name"])
+
+#  Adding & Updating
+my_dict["location"] = 'India'
+print(my_dict["location"])
+
+# Removing Items
+my_dict.pop("location")
+print(my_dict)
+# del my_dict["location"] # Removes 'location'
+#my_dict.clear()         # Removes everything
+
+print('\n')
+# Looping Through a Dictionary
+for key in my_dict:
+    print(key, my_dict[key])
+
+for key, value in my_dict.items():
+    print(f"{key} → {value}")
+
+
+
+
+
+
+
+# TASK:
+'''
+Get pull request information on a repository using python
+    algorithm:
+    requirement is to get pull request on a github repo, for that will need to write a python script.
+    so basically will write a python script to interact with github repo (api)
+    1. we need to identify module that we need and its: 'request' module.
+    2. using the moule we need to make api call
+'''
